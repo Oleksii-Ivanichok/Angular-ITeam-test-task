5dd911ada42743fecbd1c0ec241195c4cb23072d
Oleksii <oleksii.ivanichok@gmail.com>
2024-01-26 18:03:06 +0200

    completed

---
+++ src/app/_models/manageUsers.ts
@@ -0,0 +1,8 @@
+export interface ManageUsers {
+  name: string
+  lastName: string
+  dateOfBirth: string
+  education: string
+  role: string
+  position: string
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ReportsService} from "../../_services/reports.service";
+import {ReportComponent} from "../../components/report/report.component";
+import {AdminNavComponent} from "../../components/admin-nav/admin-nav.component";
+import {MatButton} from "@angular/material/button";
+import {AuthService} from "../../_services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ReportComponent, AdminNavComponent, MatButton],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  isAdmin: boolean = false;
+  constructor(public reportsService: ReportsService, public authService: AuthService, private router: Router) {
+  }
+  ngOnInit(): void {
+    this.reportsService.getReports().subscribe();
+    this.authService.isAdmin().subscribe(isAdmin => {
+      this.isAdmin = isAdmin;
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+
 
 }

f783b90f395e33086e57fb84a16d0651c297ecb0
Oleksii <oleksii.ivanichok@gmail.com>
2024-01-29 13:11:14 +0200

    deploy

--- src/app/_models/graph.ts
+++
@@ -1,6 +0,0 @@
-export interface Graph {
-  agreeableness: number
-  drive: number
-  luck: number
-  openness: number
-}
--- src/app/_models/manageUsers.ts
+++
@@ -1,8 +0,0 @@
-export interface ManageUsers {
-  name: string
-  lastName: string
-  dateOfBirth: string
-  education: string
-  role: string
-  position: string
-}
